9d5e41bc57cbdcd86969094fdd4ad0d86ceeaef7
samarthsehgal97 <samarthsehgal97@gmail.com>
2019-04-06 21:14:53 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <todo-list v-bind:todos="todos"></todo-list>
+    <create-todo v-on:add-todo="addTodo"></create-todo>
+  </div>
+</template>
+  import TodoList from './components/TodoList'
+  import CreateTodo from './components/CreateTodo'
+<script>
+  import TodoList from './components/TodoList';
+  import CreateTodo from './components/CreateTodo';
+
+  export default {
+  name: 'app',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+
+  methods: {
+      addTodo(todo) {
+        this.todos.push(todo);
+      },
+    },
+  // data function avails data to the template
+  data() {
+    return {
+      todos: [{
+        title: 'Todo A',
+        project: 'Project A',
+        done: false,
+      }, {
+        title: 'Todo B',
+        project: 'Project B',
+        done: true,
+      }, {
+        title: 'Todo C',
+        project: 'Project C',
+        done: false,
+      }, {
+        title: 'Todo D',
+        project: 'Project D',
+        done: false,
+      }],
+    };
+  },
+
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="ui segment"><div class="ui center aligned header">To-Do List</div></div>
+    <p class="tasks">Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p class="tasks">Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :todo.sync="todo"></todo>
+
+  </div>
+</template>
+
+<script type = "text/javascript" >
+  import Todo from './Todo';
+
+  export default {
+    props: ['todos'],
+    components: {
+      Todo,
+    },
+    methods: {
+      deleteTodo(todo) {
+      console.log(this.todos.indexOf(todo));
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    },
+    completeTodo(todo) {
+      console.log("todolist");
+      console.log(this.todos.indexOf(todo));
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos[todoIndex].done = true;
+    },
+  },
+  };
+</script>
+<style>
+  p.tasks{
+    text-align: center;
+  }
+</style>
